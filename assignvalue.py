e2c7a06db6dce92acbb0b46a034211a61fc8bd36
AbuthahirGitHub <abuthahir.dataset+github@gmail.com>
2024-08-07 01:13:13 +0530

    Create assignvalue.py

---
+++ assignvalue.py
@@ -0,0 +1,16 @@
+"""
+video_path = "" #Replace With your source video file
+output_folder = "" #Where all the source extracted frames needs to be stored
+input_folder = "" #Where all the source extracted frames stored
+output_folder = "" #Where all the colorMatchedFrames to Be stored
+reference_image_path = "" #Where is the reference image was stored
+input_folder = "" #Load ColorMatched Frames to convert into video
+output_video_path = "" #Load ColorMatched video needs to be saved
+"""
+source_video_path = "" #Replace With your source video file
+frame_output_folder = "" #Where all the source extracted frames needs to be stored
+frame_input_folder = "" #Where all the source extracted frames stored
+cframe_output_folder = "" #Where all the colorMatchedFrames to Be stored
+creference_image_path = "" #Where is the reference image was stored
+cframe_input_folder = "" #Load ColorMatched Frames to convert into video
+coutput_video_path = "" #Load ColorMatched video needs to be saved
